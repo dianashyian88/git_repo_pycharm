a4fe319a2c7959185a13b725071adfbd9d3063ea
dianashyian88 <dvshyian@gmail.com>
2023-05-15 23:40:59 +0300

    added funcs.py

--- funcs.py
+++ funcs.py
@@ -2,5 +2,10 @@ def sum_numbers(a, b):
     return a + b
 
 
+def substr_numbers(a, b):
+    return a - b
+
+
 if __name__ == '__main__':
     print(sum_numbers(2, 5))
+    print(substr_numbers(8, -1))
